4c2391d8e9c41054d5c334659ad1a0bb8861557d
Tatiana <Tatiana@TATIS>
2018-02-09 23:40:53 -0500

    Primer commit

---
+++ Calculadora1/src/calculadora1/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora1;
+
+/**
+ *
+ * @author Tatiana
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        //Crear un objeto de la clase Calculadora
+        Calculadora micalculadora; //Definición
+        micalculadora = new Calculadora(); //Construir el objeto
+    }
+}
